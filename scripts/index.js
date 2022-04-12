7e0214f893b60e8bdc5ecd33463d555d7addace3
Roman Chistyakov <roman.chistiakov@gmail.com>
2022-04-13 01:56:57 +0200

    hotfix/first review

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,39 @@
 const popupElement = document.querySelector('.popup');
 const editButton = document.querySelector('.profile__edit-button');
 const closeButton = popupElement.querySelector('.popup__close');
-const saveButton = popupElement.querySelector('.popup__save');
+
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
-let editName = document.querySelector('.popup__name');
-let editDescription = document.querySelector('.popup__description');
 
+let formElement = document.querySelector('.popup__edit-form')
+let editName = formElement.querySelector('#input-name');
+let editDescription = formElement.querySelector('#input-description');
+
+// Открытие попапа редактирования профиля
 function openPopup() {
   popupElement.classList.add('popup_opened');
   editName.defaultValue = profileName.textContent;
   editDescription.defaultValue = profileDescription.textContent;
 }
 
+// Закрытие попапа редактирования профиля
 function closePopup() {
   popupElement.classList.remove('popup_opened');
 }
 
-function savePopup() {
+// Отправка формы
+function formSubmitHandler(evt) {
+  evt.preventDefault();
   profileName.textContent = editName.value;
   profileDescription.textContent = editDescription.value;
   closePopup();
 }
 
+// Обработчик события кнопки Редактировать профиль
 editButton.addEventListener('click', openPopup);
 
+// Обработчик события кнопки Закрыть
 closeButton.addEventListener('click', closePopup);
 
-saveButton.addEventListener('click', savePopup);
+// Обработчик события отправки формы
+formElement.addEventListener('submit', formSubmitHandler);
